3fccfead3330ecf184322f0114fcaaf57fc86eab
audioluxdevices <audioulxdevices@gmail.com>
2017-12-13 19:09:41 -0800

    fix for audiotex library
     if (LOG_LEVEL_ERRORS <= _level) {
    		print ("ERROR: ",0);
            va_list args;
            print ("ERROR: ",args);
            va_start(args, msg);
            print(msg,args);
        }

--- Logging.cpp
+++ Logging.cpp
@@ -8,8 +8,8 @@ void Logging::Init(int level, long baud){
 
 void Logging::Error(char* msg, ...){
     if (LOG_LEVEL_ERRORS <= _level) {   
-		print ("ERROR: ",0);
         va_list args;
+        print ("ERROR: ",args);
         va_start(args, msg);
         print(msg,args);
     }
